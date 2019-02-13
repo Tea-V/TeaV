100f0aba6da87ae484ac533936aa0b2a31dbe5a2
etienne_tripier <etienne.tripier@airbnb.com>
2019-02-12 21:22:42 -0800

    Fix serverless build

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@
 const withTypescript = require('@zeit/next-typescript');
 const { withGraphQLConfig } = require('next-graphql-react/server');
 
-module.exports = withGraphQLConfig(withTypescript());
+if (process.env.NODE_ENV === 'development') {
+  module.exports = {
+    ...withGraphQLConfig(),
+    ...withTypescript(),
+  };
+} else {
+  module.exports = {
+    ...withGraphQLConfig(),
+    target: 'serverless',
+  };
+}
