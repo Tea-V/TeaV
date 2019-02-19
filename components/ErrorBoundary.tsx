a1815419290bf67fb224bdf1444c9385c8184ba0
etienne_tripier <etienne.tripier@airbnb.com>
2019-02-18 16:58:11 -0800

    Add ErrorBoundary

---
+++ components/ErrorBoundary.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+type ErrorBoundaryProps = {
+  children: React.ReactNode;
+  fallback: React.ReactNode;
+};
+type ErrorBoundaryState = Readonly<{
+  hasError: boolean;
+}>;
+
+const defaultProps = {
+  fallback: null,
+};
+
+export default class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  static defaultProps = defaultProps;
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.error(error, errorInfo);
+    }
+  }
+
+  render() {
+    const { children, fallback } = this.props;
+    const { hasError } = this.state;
+    return hasError ? fallback : children;
+  }
+}
