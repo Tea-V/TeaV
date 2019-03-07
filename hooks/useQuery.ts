32119719d1ca253529a9dc943ccbe67c753b56d4
etienne_tripier <etienne.tripier@airbnb.com>
2019-03-06 21:06:55 -0800

    Include credentials

--- hooks/useQuery.ts
+++ hooks/useQuery.ts
@@ -4,6 +4,7 @@ export default (query: string, optionsOverride = {}) =>
   useGraphQL({
     fetchOptionsOverride(options: object) {
       Object.assign(options, {
+        credentials: 'include',
         url: process.env.GRAPHQL_URL,
         ...optionsOverride,
       });
