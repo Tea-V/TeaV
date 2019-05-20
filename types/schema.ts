01bed92ee6130209fb09e5aeadc0ba69b8855376
etienne_tripier <etienne.tripier@airbnb.com>
2019-05-19 19:08:31 -0700

    Fix types

--- types/schema.ts
+++ types/schema.ts
@@ -9,34 +9,41 @@ export type Scalars = {
 };
 
 export type Connection = {
+  __typename?: 'Connection';
   pageInfo: PageInfo;
 };
 
 export type Edge = {
+  __typename?: 'Edge';
   cursor: Scalars['String'];
 };
 
 export type Movie = Node & {
+  __typename?: 'Movie';
   createdAt: Scalars['Int'];
   id: Scalars['ID'];
   title: Scalars['String'];
 };
 
 export type MoviesConnection = Connection & {
+  __typename?: 'MoviesConnection';
   edges: Array<MoviesEdge>;
   pageInfo: PageInfo;
 };
 
 export type MoviesEdge = Edge & {
+  __typename?: 'MoviesEdge';
   cursor: Scalars['String'];
   node: Movie;
 };
 
 export type Node = {
+  __typename?: 'Node';
   id: Scalars['ID'];
 };
 
 export type PageInfo = {
+  __typename?: 'PageInfo';
   endCursor?: Maybe<Scalars['String']>;
   hasNextPage: Scalars['Boolean'];
   hasPreviousPage: Scalars['Boolean'];
@@ -44,6 +51,7 @@ export type PageInfo = {
 };
 
 export type Query = {
+  __typename?: 'Query';
   movie?: Maybe<Movie>;
   movies: MoviesConnection;
 };
